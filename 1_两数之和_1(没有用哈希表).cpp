9f3ccfafa76e45db148704f1b91675b538b77652
mikkkey-hub <ma15127581139@126.com>
2026-03-06 19:01:42 +0800

    Correct formatting in two-sum solution without hash table
    Fix indentation and formatting issues in the nested loop.

--- 1_两数之和_1(没有用哈希表).cpp
+++ 1_两数之和_1(没有用哈希表).cpp
@@ -5,7 +5,7 @@ public:
         for(int i =0;i<nums.size();i++)
         {
             for (int j= i +1 ;j<nums.size();j++)//必须从下一个位置开始 ,[3,2,4]否则直接3+3了
-     122       {
+            {
                if (nums[i] + nums[j] == target) {
                     return {i, j}; //使用vector容器时可以这么写
                }
